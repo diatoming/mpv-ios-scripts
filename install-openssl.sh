42fe28d032dc4a215cc800858014da74fe67c698
Ronnie W <llqoli@gmail.com>
2023-10-10 19:07:07 +0800

    添加了 openSSL 的 Build 腳本

---
+++ install-openssl.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+OPENSSL_VERSION="1.1.1s"
+IOS_SDK_VERSION=$(xcrun -sdk iphoneos --show-sdk-version)
+TVOS_SDK_VERSION=$(xcrun -sdk appletvos --show-sdk-version)
+TVOS_FOLDER="AppleTVOS$TVOS_SDK_VERSION-arm64.sdk"
+IOS_FOLDER="iPhoneOS$IOS_SDK_VERSION-arm64.sdk"
+
+rm -rf openssl-src
+rm -rf openssl
+
+git clone https://github.com/jasonacox/Build-OpenSSL-cURL.git openssl-src/
+cd openssl-src
+
+./build.sh -o $OPENSSL_VERSION
+
+cd ..
+mkdir -p openssl/$IOS_FOLDER
+mkdir -p openssl/$TVOS_FOLDER
+cp -r ./openssl-src/openssl/iOS-fat ./openssl/$IOS_FOLDER
+cp -r ./openssl-src/openssl/tvOS-fat ./openssl/$TVOS_FOLDER
