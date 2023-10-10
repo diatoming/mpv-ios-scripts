46de16eb05231b1aed93ebfcbf2c61f4079a7bd6
Ronnie W <llqoli@gmail.com>
2023-10-10 17:15:17 +0800

    Update install-gas-preprocessor.sh

--- install-gas-preprocessor.sh
+++ install-gas-preprocessor.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -e
+
 sudo git clone https://github.com/bigsen/gas-preprocessor.git /usr/local/bin/gas
 sudo cp /usr/local/bin/gas/gas-preprocessor.pl /usr/local/bin/gas-preprocessor.pl
 sudo chmod 777 /usr/local/bin/gas-preprocessor.pl
