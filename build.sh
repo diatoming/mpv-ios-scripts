9b607c5b0b708dec26e2863ac2255a82a1a1552c
Ronnie W <llqoli@gmail.com>
2023-10-10 19:52:18 +0800

    install-openssl.sh 修正錯誤文件夾

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -e
 
+rm -rf scratch-ios scratch-tv
 ./compile.sh -p ios -e distribution && ./compile.sh -p tv -e distribution && ./xcframework.sh
--- install-openssl.sh
+++ install-openssl.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
 
-OPENSSL_VERSION="1.1.1s"
+OPENSSL_VERSION="1.1.1k"
 IOS_SDK_VERSION=$(xcrun -sdk iphoneos --show-sdk-version)
 TVOS_SDK_VERSION=$(xcrun -sdk appletvos --show-sdk-version)
 TVOS_FOLDER="AppleTVOS$TVOS_SDK_VERSION-arm64.sdk"
 IOS_FOLDER="iPhoneOS$IOS_SDK_VERSION-arm64.sdk"
 
-rm -rf openssl-src
 rm -rf openssl
+rm -rf openssl-src
 
 git clone https://github.com/jasonacox/Build-OpenSSL-cURL.git openssl-src/
 cd openssl-src
@@ -17,5 +17,5 @@ cd openssl-src
 cd ..
 mkdir -p openssl/$IOS_FOLDER
 mkdir -p openssl/$TVOS_FOLDER
-cp -r ./openssl-src/openssl/iOS-fat ./openssl/$IOS_FOLDER
-cp -r ./openssl-src/openssl/tvOS-fat ./openssl/$TVOS_FOLDER
+cp -r ./openssl-src/openssl/iOS-fat/ ./openssl/$IOS_FOLDER/
+cp -r ./openssl-src/openssl/tvOS/ ./openssl/$TVOS_FOLDER/
