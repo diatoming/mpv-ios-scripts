9de212b47dd3df6fc7cba517ab646cb6f1551a46
Ronnie W <llqoli@gmail.com>
2023-10-10 15:25:50 +0800

    DEPLOYMENT_TARGET

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-./compile.sh -p ios -e development && ./compile.sh -p ios -e distribution && ./compile.sh -p tv -e development && ./compile.sh -p tv -e distribution && ./xcframework.sh
+./compile.sh -p ios -e distribution && ./compile.sh -p tv -e distribution && ./xcframework.sh
--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 LIBRARIES="libuchardet libfribidi libfreetype libharfbuzz libass ffmpeg libmpv libssl"
 IOS_SDK_VERSION=$(xcrun -sdk iphoneos --show-sdk-version)
 TVOS_SDK_VERSION=$(xcrun -sdk appletvos --show-sdk-version)
-DEPLOYMENT_TARGET="11.0"
+DEPLOYMENT_TARGET="15.0"
 
 export PKG_CONFIG_PATH
 export LDFLAGS
@@ -16,18 +16,18 @@ export PLATFORM
 export CMAKE_OSX_ARCHITECTURES
 
 while getopts "p:e:" OPTION; do
-case $OPTION in
-		e )
-			ENVIRONMENT=$(echo "$OPTARG" | awk '{print tolower($0)}')
-			;;
-		p )
-			PLATFORM=$(echo "$OPTARG" | awk '{print tolower($0)}')
-			;;
-		? )
-			echo "Invalid option"
-			exit 1
-			;;
-	esac
+    case $OPTION in
+    e)
+        ENVIRONMENT=$(echo "$OPTARG" | awk '{print tolower($0)}')
+        ;;
+    p)
+        PLATFORM=$(echo "$OPTARG" | awk '{print tolower($0)}')
+        ;;
+    ?)
+        echo "Invalid option"
+        exit 1
+        ;;
+    esac
 done
 
 export PATH="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/:$PATH"
@@ -94,15 +94,15 @@ for ARCH in $ARCHS; do
             ALDFLAGS="-arch $ARCH -isysroot $SDKPATH $MIN_VERSION_DEVICE_LDFLAG,$DEPLOYMENT_TARGET -lbz2"
             OPENSSL="$ROOT/openssl/$PLATFORM$SDK_VERSION-arm64.sdk"
         fi
-	elif [[ $ARCH = "x86_64" ]]; then
+    elif [[ $ARCH = "x86_64" ]]; then
         HOSTFLAG="x86_64"
         CMAKE_OSX_ARCHITECTURES=$ARCH
         PLATFORM=$PLATFORM_SIMULATOR
-		export SDKPATH=$SDKPATH_SIMULATOR
-		ACFLAGS="-arch $ARCH -isysroot $SDKPATH $MIN_VERSION_SIMULATOR_CFLAG=$DEPLOYMENT_TARGET"
-		ALDFLAGS="-arch $ARCH -isysroot $SDKPATH $MIN_VERSION_SIMULATOR_LDFLAG,$DEPLOYMENT_TARGET -lbz2"
-		OPENSSL="$ROOT/openssl/$PLATFORM$SDK_VERSION-x86_64.sdk"
-	else
+        export SDKPATH=$SDKPATH_SIMULATOR
+        ACFLAGS="-arch $ARCH -isysroot $SDKPATH $MIN_VERSION_SIMULATOR_CFLAG=$DEPLOYMENT_TARGET"
+        ALDFLAGS="-arch $ARCH -isysroot $SDKPATH $MIN_VERSION_SIMULATOR_LDFLAG,$DEPLOYMENT_TARGET -lbz2"
+        OPENSSL="$ROOT/openssl/$PLATFORM$SDK_VERSION-x86_64.sdk"
+    else
         echo "Unhandled architecture option"
         exit 1
     fi
@@ -116,46 +116,47 @@ for ARCH in $ARCHS; do
     fi
     CXXFLAGS="$CFLAGS"
 
+    echo "OPENSSL PTAH -->$OPENSSL"
     CFLAGS="$CFLAGS -I$OPENSSL/include"
     LDFLAGS="$LDFLAGS -L$OPENSSL/lib"
-    
+
     mkdir -p $SCRATCH
 
     PKG_CONFIG_PATH="$SCRATCH/$ARCH-$ENVIRONMENT/lib/pkgconfig"
     COMMON_OPTIONS="--prefix=$SCRATCH/$ARCH-$ENVIRONMENT --exec-prefix=$SCRATCH/$ARCH-$ENVIRONMENT --build=x86_64-apple-darwin14 --enable-static \
                     --disable-shared --disable-dependency-tracking --with-pic --host=$HOSTFLAG"
-    
+
     for LIBRARY in $LIBRARIES; do
         case $LIBRARY in
-            "libfribidi" )
-				mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/fribidi && cd $_ && $SCRIPTS/fribidi-build
-				;;
-            "libfreetype" )
-				mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/freetype && cd $_ && $SCRIPTS/freetype-build
-				;;
-            "libharfbuzz" )
-				mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/harfbuzz && cd $_ && $SCRIPTS/harfbuzz-build
-				;;
-            "libass" )
-				mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/libass && cd $_ && $SCRIPTS/libass-build
-				;;
-            "libuchardet" )
-				mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/uchardet && cd $_ && $SCRIPTS/uchardet-build
-				;;
-            "ffmpeg" )
-				mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/ffmpeg && cd $_ && $SCRIPTS/ffmpeg-build
-				;;
-            "libmpv" )
-                if [[ "$ENVIRONMENT" = "development" ]]; then
-                    CFLAGS="$ACFLAGS -fembed-bitcode -g2 -Og"
-                    LDFLAGS="$ALDFLAGS -fembed-bitcode -g2 -Og"
-                fi
-				$SCRIPTS/mpv-build && cp $SRC/mpv*/build/libmpv.a "$SCRATCH/$ARCH-$ENVIRONMENT/lib"
-				;;
-            "libssl" )
-                cp -a $OPENSSL/include/. $SCRATCH/$ARCH-$ENVIRONMENT/include/
-                cp -a $OPENSSL/lib/. $SCRATCH/$ARCH-$ENVIRONMENT/lib/
-				;;
+        "libfribidi")
+            mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/fribidi && cd $_ && $SCRIPTS/fribidi-build
+            ;;
+        "libfreetype")
+            mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/freetype && cd $_ && $SCRIPTS/freetype-build
+            ;;
+        "libharfbuzz")
+            mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/harfbuzz && cd $_ && $SCRIPTS/harfbuzz-build
+            ;;
+        "libass")
+            mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/libass && cd $_ && $SCRIPTS/libass-build
+            ;;
+        "libuchardet")
+            mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/uchardet && cd $_ && $SCRIPTS/uchardet-build
+            ;;
+        "ffmpeg")
+            mkdir -p $SCRATCH/$ARCH-$ENVIRONMENT/ffmpeg && cd $_ && $SCRIPTS/ffmpeg-build
+            ;;
+        "libmpv")
+            if [[ "$ENVIRONMENT" = "development" ]]; then
+                CFLAGS="$ACFLAGS -fembed-bitcode -g2 -Og"
+                LDFLAGS="$ALDFLAGS -fembed-bitcode -g2 -Og"
+            fi
+            $SCRIPTS/mpv-build && cp $SRC/mpv*/build/libmpv.a "$SCRATCH/$ARCH-$ENVIRONMENT/lib"
+            ;;
+        "libssl")
+            cp -a $OPENSSL/include/. $SCRATCH/$ARCH-$ENVIRONMENT/include/
+            cp -a $OPENSSL/lib/. $SCRATCH/$ARCH-$ENVIRONMENT/lib/
+            ;;
         esac
     done
 done
